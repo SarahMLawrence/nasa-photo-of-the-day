53a883c7358a21d2ad071be759d3f51036fdf2a3
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-03-04 22:40:03 -0500

    styling

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -3,28 +3,21 @@ import React from "react";
 
 const VideoCard = props => {
 
- 
+
 
     return (
-        
-        <div class="card" style={{width: '40rem', border: 'solid', alignContent: 'center'}} >
-        <img
-            style={{marginLeft: '40px', padding: '10px'}}
-            class="card-img-top"
-            alt="photo day"
-            className="post-thumb"
-            src={props.hdurl}
-
-        />
+        <div class="card" style={{width: '60rem', marginTop: '5rem', marginLeft: '30rem', padding: '1.5rem'}} >
+        {props.media_type === "video"} 
+        <iframe width="500" height="315" src={props.src} style={{marginLeft: '15rem', padding: '20px'}}></iframe>
         <div class="card-body">
             <h1>{props.title}</h1>
             <h3>{props.date}</h3>
             <p class="card-text">{props.explanation}</p>
 
         </div>
+        
+        </div>
 
-    </div>
-       
     );
 };
 export default VideoCard;
